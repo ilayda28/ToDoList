8cf10933caf0f30b61b71b3e92dad432cc37ac3c
İlayda Sena Keleş <ilaydasenakeles28@gmail.com>
2023-03-13 22:22:02 +0300

    Start

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Text, View, SafeAreaView } from "react-native";
+ 
+function App () {
+  return(
+    <View>
+      <Text></Text>
+    </View>
+  );
+}
+
+export default App;
