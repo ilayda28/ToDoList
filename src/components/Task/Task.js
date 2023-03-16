114f306c3923cf5a14d728bde3729b1892c683cc
İlayda Sena Keleş <ilaydasenakeles28@gmail.com>
2023-03-17 01:21:02 +0300

    added remove function

--- src/components/Task/Task.js
+++ src/components/Task/Task.js
@@ -3,10 +3,13 @@ import { Text, View, SafeAreaView, TouchableOpacity } from "react-native";
 import styles from "./Task.style";
 
 const Task =(props) =>{
-    
+    const [change, setChange] = useState ( true )
+    function changeCard () {
+        setChange (!change)
+    }
     return(    
-            <TouchableOpacity style = {styles.card} >
-            <Text style= {styles.text} >{props.text}</Text>
+            <TouchableOpacity style = {change ? styles.card : styles.cardChange} onPress={changeCard}>
+            <Text style= {change ? styles.task :styles.changeText} >{props.text}</Text>
             </TouchableOpacity >   
     );
 };
